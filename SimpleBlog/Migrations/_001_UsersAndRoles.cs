828b1fd5c75675398919bef36040493656701842
stefanwapnick <stefanwapnick@hotmail.com>
2015-08-23 20:45:39 -0400

    Added migration setup

---
+++ SimpleBlog/Migrations/_001_UsersAndRoles.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FluentMigrator;
+using System.Data; 
+
+namespace SimpleBlog.Migrations
+{
+    [Migration(1)]
+    public class _001_UsersAndRoles : Migration
+    {
+        /// <summary>
+        /// Migrate to next schema version
+        /// </summary>
+        public override void Up()
+        {
+            Create.Table("users")
+                .WithColumn("user_id").AsInt32().Identity().PrimaryKey()
+                .WithColumn("username").AsString(128)
+                .WithColumn("email").AsCustom("VARCHAR(256)")
+                .WithColumn("password_hash").AsString(128);
+
+            Create.Table("roles")
+                .WithColumn("role_id").AsInt32().Identity().PrimaryKey()
+                .WithColumn("role").AsString(128);
+
+            Create.Table("roles_users")
+                .WithColumn("user_id").AsInt32().ForeignKey("users", "user_id").OnDelete(Rule.Cascade)
+                .WithColumn("role_id").AsInt32().ForeignKey("roles", "role_id").OnDelete(Rule.Cascade);
+        }
+
+        /// <summary>
+        /// Rollback to previous database schema
+        /// </summary>
+        public override void Down()
+        {
+            Delete.Table("roles_users");
+            Delete.Table("roles");
+            Delete.Table("users"); 
+        }
+    }
+}
